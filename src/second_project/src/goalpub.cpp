2100ee7ec5dd9ab622cf6124b57cf8d1b168e83d
icpmoles <8940011+icpmoles@users.noreply.github.com>
2024-05-26 10:53:54 +0200

    start actionclient for pr2 vcome dio comanda
    Signed-off-by: icpmoles <8940011+icpmoles@users.noreply.github.com>

--- src/second_project/src/goalpub.cpp
+++ src/second_project/src/goalpub.cpp
@@ -1,92 +1,38 @@
 #include <ros/ros.h>
-// generic action libs
-#include <actionlib/server/simple_action_server.h>
-// generated from .action file
-#include <second_project/SlamAction.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
 
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-class SlamAction
-{
-private:
-  ros::NodeHandle nh_;
-  // FibonacciAction pointer to itself???
-  actionlib::SimpleActionServer<second_project::SlamAction> as_; // action lib server
-  std::string action_name_;
-  // create messages that are used to published feedback/result
-  // just custom messags at the end
+int main(int argc, char** argv){
+  ros::init(argc, argv, "simple_navigation_goals");
 
-  second_project::SlamActionFeedback feedback_;
-  second_project::SlamActionResult result_;
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
 
-public:
-  // executeCB = actual callback, this is just a dispatcher of sort
-  SlamAction(std::string name) :
-    as_(nh_, name, boost::bind(&SlamAction::executeCB, this, _1), false), // autostart false
-    action_name_(name)
-  {
-    as_.start(); // we start it manually
+  //wait for the action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
   }
 
-  ~SlamAction(void)
-  {
-  }
+  move_base_msgs::MoveBaseGoal goal;
 
+  //we'll send a goal to the robot to move 1 meter forward
+  goal.target_pose.header.frame_id = "base_link";
+  goal.target_pose.header.stamp = ros::Time::now();
 
+  goal.target_pose.pose.position.x = 1.0;
+  goal.target_pose.pose.orientation.w = 1.0;
 
-  void executeCB(const second_project::SlamActionGoalConstPtr &goal)
-  {
-    // helper variables
-    ros::Rate r(1); //simulate compute time
-    bool success = true;
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
 
-    // clear and set first two values
-    feedback_.sequence.clear();
-    feedback_.sequence.push_back(0);
-    feedback_.sequence.push_back(1);
-    // feeback is just an array in .action
+  ac.waitForResult();
 
-
-    // publish info to the console for the user
-    ROS_INFO("%s: Executing, creating fibonacci sequence of order %i with seeds %i, %i", action_name_.c_str(), goal->order, feedback_.sequence[0], feedback_.sequence[1]);
-
-    // start executing the action
-    for(int i=1; i<=goal->order; i++)
-    {
-      // check that preempt has not been requested by the client
-      if (as_.isPreemptRequested() || !ros::ok())
-      {
-        ROS_INFO("%s: Preempted", action_name_.c_str());
-        // set the action state to preempted
-        as_.setPreempted();
-        success = false;
-        break;
-      }
-      feedback_.sequence.push_back(feedback_.sequence[i] + feedback_.sequence[i-1]);
-      // publish the feedback
-      as_.publishFeedback(feedback_);
-      // this sleep is not necessary, we simulate compute time
-      r.sleep();
-    }
-
-    if(success)
-    {
-      result_.sequence = feedback_.sequence;
-      ROS_INFO("%s: Succeeded", action_name_.c_str());
-      // set the action state to succeeded
-      as_.setSucceeded(result_);
-    }
-  }
-
-
-};
-
-
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "goalserver");
-
-  SlamAction points("gothere");
-  ros::spin();
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("Hooray, the base moved 1 meter forward");
+  else
+    ROS_INFO("The base failed to move forward 1 meter for some reason");
 
   return 0;
 }
